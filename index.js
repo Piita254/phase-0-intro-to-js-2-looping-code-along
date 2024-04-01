1bb7629967d0e11593281d2285edef53ad7c9e19
Peter Ndungu <petersonndungu995@gmail.com>
2024-04-01 18:19:43 +0300

    “completed”

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+// Function to generate thank you messages
+function writeCards(names, event) {
+    let messages = [];
+    for (let name of names) {
+        messages.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+    }
+    return messages;
+}
+
+// Function to count down from a given number
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
